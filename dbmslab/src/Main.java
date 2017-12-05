7e3e134c71cd6dba6ba4d0aa07e881e477963624
genesissoriano <2161745@slu.edu.ph>
2017-12-05 22:09:26 +0800

    Revert "hehe"
    This reverts commit 443fa5146e762956e75d5501d25e043abfce25a5.

--- dbmslab/src/Main.java
+++ dbmslab/src/Main.java
@@ -19,23 +19,7 @@ public class Main {
     
     public static void showMenu() {
         System.out.println("The Vet Clinic");
-		System.out.println("[1] Display all the appointments");
-        System.out.println("[2] Add a new veterinarian");
-        System.out.println("[3] Update a veterinarian's type");
-        System.out.println("[4] Delete a veterinarian");
-        System.out.println("[5] Create new appointment for a veterinarian");
-        System.out.println("[6] Display veterinarians and their appointments");
-        System.out.println("[7] Update a vet's appointment time");
-        System.out.println("[8] Delete a veterinarian's appointment");
-        System.out.println("[9] Register a new pet patient");
-        System.out.println("[10] Delete a pet patient in record");
-        System.out.println("[11] Display the total number of appointments of a certain pet");
-        System.out.println("[12] Update a pet's appointment");
-        System.out.println("[13] Delete a pet's appointment");
-        System.out.println("[14] Display all owners who has a specific type of a species as a pet");
-        System.out.println("[15] Change owner of pet");
-        System.out.println("[16] Update owner's id");
-	}
+    }
     
     public static void main(String[] args) {
         // connectToDatabase();

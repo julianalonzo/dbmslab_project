80d5433ba5e98577d3575e6b7ce4dabcb04f6a58
genesissoriano <2161745@slu.edu.ph>
2017-12-06 08:45:11 +0800

    Added method heading for add Veterinarian

--- dbmslab/src/Main.java
+++ dbmslab/src/Main.java
@@ -41,6 +41,11 @@ public class Main {
         
     }
     
+     public static void addVeterinarian(){
+    
+    }
+
+   
     public static void main(String[] args) {
         // connectToDatabase();
         showMenu();

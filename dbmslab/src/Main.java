a015573e0e8b10581422f0abc446d99ebb182218
genesissoriano <2161745@slu.edu.ph>
2017-12-06 09:01:36 +0800

    Added method heading for delete Veterinarian

--- dbmslab/src/Main.java
+++ dbmslab/src/Main.java
@@ -48,6 +48,10 @@ public class Main {
      public static void updateVetType(){
      
      }
+     
+     public static void deleteVet(){
+     
+     }
    
     public static void main(String[] args) {
         // connectToDatabase();

78f3a6a8db43bc0553ab6bae6c544296657ef5ab
julz <julianaubreya@gmail.com>
2017-12-06 23:55:28 +0800

    Removed duplicates and added menu for finishing appointment

--- dbmslab/src/Main.java
+++ dbmslab/src/Main.java
@@ -28,30 +28,30 @@ public class Main {
         System.out.println("[5] Book a new appointment for a veterinarian");
         System.out.println("[6] Display veterinarians and their appointments");
         System.out.println("[7] Update a vet's appointment time");
-        System.out.println("[8] Cancel a veterinarian's appointment");
+        System.out.println("[8] Cancel an appointment");
         System.out.println("[9] Register a new pet patient");
         System.out.println("[10] Delete a pet patient in record");
         System.out.println("[11] Display the total number of appointments of a "
                 + "certain pet");
         System.out.println("[12] Update a pet's appointment");
-        System.out.println("[13] Cancel a pet's appointment");
-        System.out.println("[14] Display all owners who has a specific type of a"
+        System.out.println("[13] Display all owners who has a specific type of a"
                 + " species as a pet");
-        System.out.println("[15] Change owner of pet");
-        System.out.println("[16] Update owner's id");
-        System.out.println("[17] Add a new pet of an existing owner");
-        System.out.println("[18] View appointment information");
+        System.out.println("[14] Change owner of pet");
+        System.out.println("[15] Update owner's id");
+        System.out.println("[16] Add a new pet of an existing owner");
+        System.out.println("[17] View appointment information");
+        System.out.println("[18] Finish an appointment")
         System.out.println("[19] Quit");
     }
 
     public static void main(String[] args) {
         // connectToDatabase();
 
-        int choice = 19;
+        int choice = 18;
         do {
             showMenu();
             System.out.print("\nEnter choice: ");
             choice = Integer.parseInt(console.nextLine());
-        } while (choice != 19);
+        } while (choice != 18);
     }
 }

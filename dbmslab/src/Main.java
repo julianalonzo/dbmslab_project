b320f3c97774b7c8a3d06816ac9d46325f2025b3
julian <2161495@slu.edu.ph>
2017-12-14 22:24:36 +0800

    Edited Main.java. Formatted the code a bit.

--- dbmslab/src/Main.java
+++ dbmslab/src/Main.java
@@ -2,9 +2,9 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
-    // Please change dbmslab into the appropriate name of the schema
-    private static String connectionUrl = "jdbc:mysql://localhost:3306/"
-            + "dbmslab?user=root&password=";
+    // Make sure that the URL is correct
+    private static String connectionUrl = "jdbc:mysql://localhost:8889/"
+                                          + "vetclinic?user=root&password=root";
     private static Connection CONN;
     private static Scanner console = new Scanner(System.in);
 
@@ -12,13 +12,14 @@ public class Main {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             CONN = DriverManager.getConnection(connectionUrl);
+            System.out.println("Connected to the database...");
         } catch (ClassNotFoundException cne) {
             cne.printStackTrace();
         } catch (SQLException sqle) {
             sqle.printStackTrace();
         }
     }
-
+    
     public static void showMenu() {
         System.out.println("The Vet Clinic");
         System.out.println("[1] Display all the appointments of the day");
@@ -32,26 +33,121 @@ public class Main {
         System.out.println("[9] Register a new pet patient");
         System.out.println("[10] Delete a pet patient in record");
         System.out.println("[11] Display the total number of appointments of a "
-                + "certain pet");
+                            + "certain pet");
         System.out.println("[12] Update a pet's appointment");
         System.out.println("[13] Display all owners who has a specific type of a"
-                + " species as a pet");
+                            + " species as a pet");
         System.out.println("[14] Change owner of pet");
         System.out.println("[15] Update owner's id");
         System.out.println("[16] Add a new pet of an existing owner");
         System.out.println("[17] View appointment information");
-        System.out.println("[18] Finish an appointment")
+        System.out.println("[18] Finish an appointment");
         System.out.println("[19] Quit");
     }
+    
+    public static void displayPetSpecies() throws SQLException {
+        System.out.println("Enter pet species: ");
+        String input = console.nextLine();
+        String query = "SELECT * FROM pet WHERE species ='" + input + "'";
 
-    public static void main(String[] args) {
-        // connectToDatabase();
+        Statement statement = null;
+        try {
+            statement = CONN.createStatement();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        ResultSet rs = statement.executeQuery(query);
 
+        rs.beforeFirst();
+        while (rs.next()) {
+            int petid = rs.getInt(1);
+            String petname = rs.getString("pet_name");
+            String species = rs.getString("species");
+            String gender = rs.getString("gender");
+            String birthdate = rs.getString("birthdate");
+
+            System.out.printf("%3d %-25s %-25s %-25s %-25s\n", 
+                              petid, petname, species, gender, birthdate);
+        }
+    }
+    
+    public static void main(String[] args) throws SQLException {
+        
+        connectToDatabase();
+        
         int choice = 19;
         do {
             showMenu();
             System.out.print("\nEnter choice: ");
             choice = Integer.parseInt(console.nextLine());
+            
+            switch (choice) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                    
+                case 3:
+                    
+                    break;
+                    
+                case 4:
+                    
+                    break;
+                    
+                case 5:
+                    
+                    break;
+                case 6:
+                    
+                    break;
+                case 7:
+                    
+                    break;
+                case 8:
+                    
+                    break;
+                case 9:
+                    
+                    break;
+                case 10:
+                    
+                    break;
+                case 11:
+                    
+                    break;
+                case 12:
+                    
+                    break;
+                case 13:
+                    
+                    break;
+                case 14:
+                    displayPetSpecies();
+                    break;
+                case 15:
+                    
+                    break;
+                case 16:
+                    
+                    break;
+                case 17:
+                    
+                    break;
+                case 18:
+                    
+                    break;
+                case 19:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+                    break;
+                    
+            }
+            
         } while (choice != 19);
     }
 }
